feb8ff0fc58ee5c1c34c0b0a84053fb9891bc49c
Enio Schaefer <enio100schaefer@hotmail.com>
2023-02-14 16:16:13 -0300

    [Feat] :rocket: Requisito 01 concluido

--- src/routes/productsRoutes.js
+++ src/routes/productsRoutes.js
@@ -1,8 +1,16 @@
 const express = require('express');
-const { findAll } = require('../models/products.model');
+const { findAll, findById } = require('../models/products.model');
 
 const router = express.Router();
 
 router.get('/', async (_req, res) => res.status(200).json(await findAll()));
+
+router.get('/:id', async (req, res) => {
+  const { id } = req.params;
+
+  const result = await findById(id);
+  if (result) return res.status(200).json(result);
+  return res.status(404).json({ message: 'Product not found' });
+});
 
 module.exports = router;
